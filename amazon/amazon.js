895e5389f8a149a189141c8de025380929eb811f
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2017-05-01 18:53:19 -0400

    init

---
+++ amazon/amazon.js
@@ -0,0 +1,76 @@
+var mysql = require('mysql');
+var config = require('./config');
+var config = require('./products');
+var credentials = {
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '',
+  database: 'amazon'
+};
+
+var connection = mysql.createConnection(credentials);
+
+connection.connect(function(err){
+  if (err) throw err;
+  console.log(`connected at ${connection.threadId}`);
+});
+
+//populate db
+// console.log(products)
+
+//print all songs in db to the terminal
+
+
+
+var products = [{
+  product_name: 'posterboard white',
+  department_name: 'stationary',
+  price: 2.79,
+  stock_count: 67
+}, {
+  product_name: 'permanent markers thin tip black',
+  department_name: 'stationary',
+  price: 1.79,
+  stock_count: 104
+}, {
+  product_name: 'packing tape clear 400ft',
+  department_name: 'stationary',
+  price: 4.29,
+  stock_count: 13
+}, {
+  product_name: 'glitter 3 pack assorted',
+  department_name: 'stationary',
+  price: 2.39,
+  stock_count: 7
+}, {
+  product_name: 'glue white school',
+  department_name: 'stationary',
+  price: 1.29,
+  stock_count: 133
+}, {
+  product_name: 'twine 400 ft natural',
+  department_name: 'stationary',
+  price: 5.19,
+  stock_count: 17
+}, {
+  product_name: 'permanent markers wide tip black',
+  department_name: 'stationary',
+  price: 4.29,
+  stock_count: 5
+}, {
+  product_name: 'stapler rapid model 2730',
+  department_name: 'tools',
+  price: 24.29,
+  stock_count: 7
+}, {
+  product_name: 'stapler rapid model 2730',
+  department_name: 'tools',
+  price: 12.59,
+  stock_count: 3
+}, {
+  product_name: 'wooden stakes 4ft',
+  department_name: 'lumber',
+  price: 0.59,
+  stock_count: 27
+}];
